f567df1cc8d68c4622b67e2a4d289dc49dedd73d
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-09-25 18:32:12 +0700

    Add files via upload

---
+++ Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika;
+
+/**
+ *
+ * @author Lenovo
+ * Nama : Tassyakur Pasya
+ * Kelas : IF-01
+ * NIM : 10118009
+ * Matkul : PBO-01
+ * Deskripsi Program : Logika
+ */
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+    
+}
